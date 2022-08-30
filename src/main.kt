9557fe5a39543d6db3ae2e9b620e546d0cbbcf4c
gusttavosouza <gustavorobertotb@outlook.com>
2022-08-29 22:04:22 -0300

    feat: initial commit

---
+++ src/main.kt
@@ -0,0 +1,16 @@
+package src
+
+fun main(){
+    println("Hello Word")
+
+    var name = "Gustavo"
+    var age = 13;
+    var test: Boolean = true;
+
+    val test2: String = "VAMOSSSS";
+
+    println(Byte.MAX_VALUE)
+
+    println(Byte.MIN_VALUE)
+
+}
