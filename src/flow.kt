007fbea7396765e90e54581879ac7f3dcae5bb01
gusttavosouza <gustavorobertotb@outlook.com>
2022-08-30 23:44:19 -0300

    feat: flow control

---
+++ src/flow.kt
@@ -0,0 +1,12 @@
+package  src
+
+fun maiorDeIdade(age: Int) = if(age >= 18){
+    println("Maior de Idade")
+}else {
+    println("Não é maior de idade")
+}
+
+
+fun main () {
+    maiorDeIdade(18)
+}
